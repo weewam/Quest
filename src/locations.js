224e37209dbe5f6c2c7beea864de2794787bccaa
Ibz <ibrahim.asfadai@gmail.com>
2017-11-14 14:12:40 +0100

    Add template locations

---
+++ src/locations.js
@@ -0,0 +1,62 @@
+export const locations = [
+	{
+		name: 'Gröna Lund',
+		adress: 'Lilla Allmänna Gänd 9'
+		image: 'https://i.imgur.com/6kpBgPB.jpg',
+		coords : { lat : 59.323376, long: 18.096395 }
+	},
+	{
+		name: 'Ocean Bus',
+		adress: 'Strömgatan 10'
+		image: 'https://i.imgur.com/swWillV.jpg',
+		coords : { lat : 59.329592, long: 18.070607 }
+	},
+	{
+		name: 'Takvandring',
+		adress: 'Munkbron 1'
+		image: 'https://i.imgur.com/V14C7ma.jpg',
+		coords : { lat : 59.325312, long: 18.066684 }
+	},
+	{
+		name: 'Brunnsviken',
+		adress: 'Haga, Solna'
+		image: 'https://i.imgur.com/5sDHCIm.jpg',
+		coords : { lat : 59.368118, long: 18.038994 }
+	},
+	{
+		name: 'Drottningholms slott',
+		adress: 'Drottningholm'
+		image: 'https://i.imgur.com/22eCOsI.jpg',
+		coords : { lat : 59.321807, long: 17.886814 }
+	},
+	{
+		name: 'Cajsa Warg',
+		adress: 'St Eriksplan 2'
+		image: 'https://i.imgur.com/tgk3Iid.jpg',
+		coords : { lat : 59.339975, long: 18.038254 }
+	},
+	{
+		name: 'Globen',
+		adress: 'Globen'
+		image: 'https://i.imgur.com/zTEPMW0.jpg',
+		coords : { lat : 59.293556, long: 18.083561 }
+	},
+	{
+		name: 'Dramaten',
+		adress: 'Nybrogatan 2'
+		image: 'https://i.imgur.com/2GqdXRf.jpg',
+		coords : { lat : 59.333184, long: 18.076914 }
+	},
+	{
+		name: 'Kaknästorget',
+		adress: 'Mörka Kroken 28'
+		image: 'https://i.imgur.com/LpnUZo5.jpg',
+		coords : { lat : 59.335011, long: 18.126400 }
+	},
+	{
+		name: 'Bondens Marknad',
+		adress: 'Katarina Bangata'
+		image: 'https://i.imgur.com/KXi7v8s.jpg',
+		coords : { lat : 59.310336, long: 18.084300 }
+	},
+]
