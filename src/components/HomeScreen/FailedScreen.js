4a8f3eab9fdf198a13d2233ef0bc26d49b0c0e5b
杜宜蓓 <yipeitu@n133-p222.eduroam.kth.se>
2017-11-17 12:04:28 +0100

    Add FailedScreen and SuccessScreen

---
+++ src/components/HomeScreen/FailedScreen.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+
+
+
+class FailedScreen extends Component{
+  constructor(props) {
+      super(props)
+   }
+   render() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        THIS IS THE Failed Screen
+      </Text>
+      <Button title={"Go back"}
+              style={{ marginTop: 10 }}
+              onPress={() => this.props.navigation.navigate("HomeScreen")}>
+              <Text>Go Back</Text>
+            </Button>
+    </View>
+  );
+}
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(215, 150, 140)',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default FailedScreen
